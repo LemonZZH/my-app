61cdd3b7abd18da80a22172070156b3c45e9d419
LemonZZH <zzh1315414831@163.com>
2019-04-20 16:19:31 +0800

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app frist git app';
+  title = 'my-app frist 第一个自己上传的git';
 }
